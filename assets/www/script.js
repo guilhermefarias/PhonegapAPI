972c9c3e16f224cce48754639756acaaee1d44b1
Guilherme Farias <guilherme@guiky.com.br>
2012-11-09 16:07:32 -0300

    adding connection. contacts and device basic examples

--- assets/www/script.js
+++ assets/www/script.js
@@ -6,22 +6,38 @@ var application = {
 			switch(view){
 				case 'accelerometer':
 					viewUrl = 'view/accelerometer.html';
+					Zepto('.content').load(viewUrl);
 					api.accelerometer.setup();
 					break;
 				case 'camera':
 					viewUrl = 'view/camera.html';
-					api.camera.capturePhoto();				
+					Zepto('.content').load(viewUrl);
+					api.camera.capturePhoto();
 					break;
 				case 'compass':
 					viewUrl = 'view/compass.html';
-					api.compass.setup();				
+					Zepto('.content').load(viewUrl);
+					api.compass.setup();
+					break;
+				case 'connection':
+					viewUrl = 'view/connection.html';
+					Zepto('.content').load(viewUrl);
+					api.connection.setup();
+					break;
+				case 'contacts':
+					viewUrl = 'view/contacts.html';
+					Zepto('.content').load(viewUrl);
+					api.contacts.setup();
+					break;
+				case 'device':
+					viewUrl = 'view/device.html';
+					Zepto('.content').load(viewUrl);
+					api.device.setup();
 					break;
 				default:
 					alert('ainda não foi implementado :(');
-					return false;
 					break;
 			}
-			Zepto('.content').load(viewUrl);
 		});
 	}
 };
@@ -54,7 +70,7 @@ var api = {
 				quality: 50,
 				destinationType: navigator.camera.DestinationType.DATA_URL
 			};
-			navigator.camera.getPicture(api.camera.onPhotoDataSuccess, api.error, options); 	
+			navigator.camera.getPicture(api.camera.onPhotoDataSuccess, api.error, options);
 		}
 	},
 	compass: {
@@ -66,6 +82,43 @@ var api = {
 		},
 		onSuccess: function(heading){
 			Zepto('.heading span').text(Math.round(heading.magneticHeading));
+		}
+	},
+	connection: {
+		setup: function(){
+			var networkState = navigator.connection.type;
+			var states = {};
+			states[Connection.UNKNOWN]  = 'Unknown connection';
+		    states[Connection.ETHERNET] = 'Ethernet connection';
+		    states[Connection.WIFI]     = 'WiFi connection';
+		    states[Connection.CELL_2G]  = 'Cell 2G connection';
+		    states[Connection.CELL_3G]  = 'Cell 3G connection';
+		    states[Connection.CELL_4G]  = 'Cell 4G connection';
+		    states[Connection.NONE]     = 'No network connection';
+		    Zepto('.connection span').text(states[networkState]);
+		}
+	},
+	contacts: {
+		setup: function() {
+			var options = new ContactFindOptions();
+			var fields = ["displayName"];
+			navigator.contacts.find(fields, api.contacts.onSuccess, api.error, options);
+		},
+		onSuccess: function(contacts){
+			for (var i=0; i<contacts.length; i++) {
+				Zepto('ul').append('<li>' + contacts[i].displayName + '</li>')
+			}
+		}
+	},
+	device: {
+		setup: function(){
+			setTimeout(function(){
+				Zepto('.name').text(window.device.name);
+				Zepto('.cordova').text(window.device.cordova);
+				Zepto('.platform').text(window.device.platform);
+				Zepto('.uuid').text(window.device.uuid);
+				Zepto('.version').text(window.device.version);
+			}, 500);
 		}
 	}
 };
